cb736871e27f0af1189e061bb6b6dce51043a8bb
Mitchell Sibal <mitchell.sibal@gmail.com>
2021-05-08 09:10:09 -0700

    added more tests and changed App title to match future pages

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <title>Countdown 2 Deforestation</title>
+      <title>The College Life Cycles</title>
       <h1>Countdown 2 Deforestation</h1>
       <Count deadline="August 20, 2021" name="Molly"/>
       <Count deadline="August 10 2021" name="Mitchell"/>
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+
+test('Mitchell', () => {
   render(<App />);
   const linkElement = screen.getByText(/Mitchell/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('Taylor', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Taylor/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+test('Molly', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Molly/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
