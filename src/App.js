c2f84a126e834ab0478803a72e71f7b36c00b953
Mitchell Sibal <mitchell.sibal@gmail.com>
2021-05-08 08:26:16 -0700

    messing with the font size

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Countdown 2 deforestation</h1>
+      <title>Countdown 2 Deforestation</title>
+      <h1>Countdown 2 Deforestation</h1>
       <Count deadline="August 20, 2021" name="Molly"/>
       <Count deadline="August 10 2021" name="Mitchell"/>
       <Count deadline="July 27, 2021" name="Taylor"/>
